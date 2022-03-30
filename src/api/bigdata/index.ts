033fcadf9b1a5d02fba3cd8cf27d1a746902bdbb
13891353386 <1074443738@qq.com>
2022-03-30 15:46:45 +0800

    修复了一些bug

--- src/api/bigdata/index.ts
+++ src/api/bigdata/index.ts
@@ -1,7 +1,7 @@
 import request from "../../config/http"
 
 //登录
-export function dataListApi(params: any) {
+export function dataListApi(params?: any) {
     return request({
         url: '/api/keqiao/Cultivation/dataList',
         method: 'post',
